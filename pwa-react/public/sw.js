4f8673a014947bcfe34974e2fb48d0d2a1554e2f
akhil <akhil2448@gmail.com>
2026-04-11 22:16:33 -0500

    updated sw.js to suit ios push notifications

--- pwa-react/public/sw.js
+++ pwa-react/public/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener("push", function (event) {
+self.addEventListener("push", (event) => {
   let data = {};
 
   try {
@@ -9,9 +9,12 @@ self.addEventListener("push", function (event) {
 
   const notif = data.notification || data;
 
-  self.registration.showNotification(notif.title || "Notification", {
-    body: notif.body || "No message",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-  });
+  event.waitUntil(
+    self.registration.showNotification(notif.title || "Notification", {
+      body: notif.body || "No message",
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      data: notif.data || {},
+    }),
+  );
 });

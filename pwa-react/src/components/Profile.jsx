f73c7dcde88e781b417da23a466431b8619cb60d
akhil <akhil2448@gmail.com>
2026-04-15 21:49:43 -0500

    ...

--- pwa-react/src/components/Profile.jsx
+++ pwa-react/src/components/Profile.jsx
@@ -45,18 +45,15 @@ export default function Profile({
   useEffect(() => {
     if (isEditingTime) {
       document.body.style.overflow = "hidden";
-      document.body.style.position = "fixed";
-      document.body.style.width = "100%";
+      document.body.style.touchAction = "none";
     } else {
       document.body.style.overflow = "";
-      document.body.style.position = "";
-      document.body.style.width = "";
+      document.body.style.touchAction = "";
     }
 
     return () => {
       document.body.style.overflow = "";
-      document.body.style.position = "";
-      document.body.style.width = "";
+      document.body.style.touchAction = "";
     };
   }, [isEditingTime]);
 
